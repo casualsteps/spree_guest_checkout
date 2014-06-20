30884295b219a4fd6524d6a18823f5442bbf2a67
JonghunYu <yuzong@naver.com>
2014-06-20 14:22:01 -0700

    override Spree::UserRegistrationsController create method

---
+++ lib/controllers/frontend/spree/user_registrations_controller_decorator.rb
@@ -0,0 +1,17 @@
+Spree::UserRegistrationsController.class_eval do
+  def create
+    logger.debug "yuzong create"
+    @user = build_resource(spree_user_params)
+    if resource.save
+      set_flash_message(:notice, :signed_up)
+      sign_in(:spree_user, @user)
+      session[:spree_user_signup] = true
+      associate_user
+      respond_with resource, location: after_sign_up_path_for(resource)
+    else
+      clean_up_passwords(resource)
+      render :new
+    end 
+    logger.debug "yuzong create"
+  end
+end
