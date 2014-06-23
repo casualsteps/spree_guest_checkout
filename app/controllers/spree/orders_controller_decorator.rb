eacaa97e247db812b96da50bfc7c07e1448080fb
JonghunYu <yuzong@naver.com>
2014-06-23 16:10:19 -0700

    overrides orders_controller methods(show, check_authrization)

---
+++ app/controllers/spree/orders_controller_decorator.rb
@@ -0,0 +1,17 @@
+Spree::OrdersController.class_eval do
+  def show
+    @order = Spree::Order.find_by_number!(params[:id])
+  end
+
+  private
+    def check_authorization
+      session[:access_token] = params[:token] if params[:token]
+      order = Spree::Order.find_by_number(params[:id]) || current_order
+
+      if order
+        authorize! :edit, order, session[:access_token]
+      else
+        authorize! :create, Spree::Order.new
+      end
+    end
+end
