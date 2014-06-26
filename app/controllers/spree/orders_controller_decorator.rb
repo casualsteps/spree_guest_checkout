f64a54219f055d30db9ebffa2ce942abdb3d3ac0
JonghunYu <yuzong@naver.com>
2014-06-25 19:35:59 -0700

    modify authorization of an order using cookie storing access token

--- app/controllers/spree/orders_controller_decorator.rb
+++ app/controllers/spree/orders_controller_decorator.rb
@@ -5,6 +5,7 @@ Spree::OrdersController.class_eval do
 
   private
     def check_authorization
+      session[:access_token] = cookies.signed[:access_token] if cookies.signed[:access_token]
       session[:access_token] = params[:token] if params[:token]
       order = Spree::Order.find_by_number(params[:id]) || current_order
 
