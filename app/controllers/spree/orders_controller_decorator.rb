d0a5f298f7a49b82542f09cfd0acc8e174688022
JonghunYu <yuzong@naver.com>
2014-06-26 15:25:36 -0700

    let check there is shipping address id in parameters

--- app/controllers/spree/orders_controller_decorator.rb
+++ app/controllers/spree/orders_controller_decorator.rb
@@ -1,13 +1,13 @@
 Spree::OrdersController.class_eval do
-  def show
-    @order = Spree::Order.find_by_number!(params[:id])
-  end
-
   private
     def check_authorization
-      session[:access_token] = cookies.signed[:access_token] if cookies.signed[:access_token]
       session[:access_token] = params[:token] if params[:token]
       order = Spree::Order.find_by_number(params[:id]) || current_order
+
+      #if url has ship_address_id we don't need token because it comes from the link in order confirm E-mail
+      if params[:ship_address_id] == order.ship_address_id
+        session[:access_token] = order.token
+      end
 
       if order
         authorize! :edit, order, session[:access_token]
