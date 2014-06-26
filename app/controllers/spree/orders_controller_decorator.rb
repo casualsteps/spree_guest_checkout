f8d0d2ceb190bb607b76baf181f314cb3f338fbb
JonghunYu <yuzong@naver.com>
2014-06-26 15:59:31 -0700

    convert order's ship_address_id to string to compare with parameters

--- app/controllers/spree/orders_controller_decorator.rb
+++ app/controllers/spree/orders_controller_decorator.rb
@@ -1,18 +1,18 @@
 Spree::OrdersController.class_eval do
-  private
-    def check_authorization
-      session[:access_token] = params[:token] if params[:token]
-      order = Spree::Order.find_by_number(params[:id]) || current_order
 
-      #if url has ship_address_id we don't need token because it comes from the link in order confirm E-mail
-      if params[:ship_address_id] == order.ship_address_id
-        session[:access_token] = order.token
-      end
+  def check_authorization
+    session[:access_token] = params[:token] if params[:token]
+    order = Spree::Order.find_by_number(params[:id]) || current_order
 
-      if order
-        authorize! :edit, order, session[:access_token]
-      else
-        authorize! :create, Spree::Order.new
-      end
+    #if url has ship_address_id we don't need token because it comes from the link in order confirm E-mail
+    if params[:ship_address_id] == order.ship_address_id.to_s
+      session[:access_token] = order.token
     end
+
+    if order
+      authorize! :edit, order, session[:access_token]
+    else
+      authorize! :create, Spree::Order.new
+    end
+  end
 end
