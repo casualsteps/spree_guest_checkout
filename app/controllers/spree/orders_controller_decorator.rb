5fd3a4d892fab73f14f05df4c3d205f8fbd56737
James Martin Duffy <jamesmartinduffy@gmail.com>
2014-11-20 20:33:33 +0900

    Allow guests to look up order with their email

--- app/controllers/spree/orders_controller_decorator.rb
+++ app/controllers/spree/orders_controller_decorator.rb
@@ -5,8 +5,11 @@ Spree::OrdersController.class_eval do
     order = Spree::Order.find_by_number(params[:id]) || current_order
 
     #if url has ship_address_id we don't need token because it comes from the link in order confirm E-mail
-    if order != nil and params[:ship_address_id] == order.ship_address_id.to_s
+    if order != nil and (params[:email] == order.email or params[:ship_address_id] == order.ship_address_id.to_s)
       session[:access_token] = order.token
+    else
+      flash[:error] = "입력하신 정보가 정확하지 않습니다"
+      redirect_to login_path and return
     end
 
     if order
