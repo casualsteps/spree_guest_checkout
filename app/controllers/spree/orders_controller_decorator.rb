162bc4658b6db2574015c17bcfbf6c53e7d9656e
Blaž Hrastnik <blaz.hrast@gmail.com>
2015-05-21 16:52:39 +0900

    Copy upstream method

--- app/controllers/spree/orders_controller_decorator.rb
+++ app/controllers/spree/orders_controller_decorator.rb
@@ -1,18 +1,18 @@
 Spree::OrdersController.class_eval do
 
   def check_authorization
-    session[:access_token] = params[:token] if params[:token]
+    cookies.permanent.signed[:guest_token] = params[:token] if params[:token]
     order = Spree::Order.find_by_number(params[:id]) || current_order
 
     #if url has ship_address_id we don't need token because it comes from the link in order confirm E-mail
     if order != nil and params[:ship_address_id] == order.ship_address_id.to_s
-      session[:access_token] = order.guest_token
+      cookies.signed[:guest_token] = order.guest_token
     end
 
     if order
-      authorize! :edit, order, session[:access_token]
+      authorize! :edit, order, cookies.signed[:guest_token]
     else
-      authorize! :create, Spree::Order.new
+      authorize! :create, Spree::Order
     end
   end
 end
