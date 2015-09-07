e3e6c259ee516425954f6c90fc2d419ac9c1805c
MIRiyA <miriya.lee@gmail.com>
2015-09-07 12:27:40 +0900

    disable unused override

--- app/overrides/guest_checkout.rb
+++ app/overrides/guest_checkout.rb
@@ -2,5 +2,5 @@ Deface::Override.new( :virtual_path => 'spree/orders/show',
                       :name         => 'add_registration_form_in_order_complete_page',
                       :insert_bottom=> '.result-item',
                       :partial      => 'spree/orders/reg_after_order',
-                      :disabled     => false,
+                      :disabled     => true,
                       :original     => '22fcb5cfe62977684eb18c70593bd659d31a8859')
